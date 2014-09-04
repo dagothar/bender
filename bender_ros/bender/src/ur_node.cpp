2e0a78d6fda36ea404969ee31dec0d667af29f63
dagothar <dagothar@gmail.com>
2014-09-04 16:40:20 +0200

    added command line parameters to ur_node + loading configuration from XML

--- bender_ros/bender/src/ur_node.cpp
+++ bender_ros/bender/src/ur_node.cpp
@@ -12,7 +12,15 @@
 #include <boost/program_options/variables_map.hpp>
 #include <boost/program_options/option.hpp>
 #include <boost/program_options/parsers.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+#include <boost/optional.hpp>
+#include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/algorithm/string.hpp>
 #include <bender/Q.h>
+#include <bender/URGetQ.h>
 #include <bender/URStop.h>
 #include <bender/URMoveToQ.h>
 #include <bender/URMoveToT.h>
@@ -23,11 +31,26 @@
 USE_ROBWORK_NAMESPACE;
 using namespace rw;
 using namespace boost::program_options;
-namespace po = boost::program_options;
+using namespace boost::numeric;
+using namespace boost::property_tree;
 
 
 
 rwhw::URCallBackInterface URInterface;
+
+
+
+/**
+ * ur_get_q service
+ */
+bool ur_get_q(bender::URGetQ::Request& req, bender::URGetQ::Response& res)
+{
+	ROS_INFO("Requested URGetQ");
+	
+	// perform service
+	
+	return true;
+}
 
 
 
@@ -51,7 +74,7 @@ bool ur_stop(bender::URStop::Request& req, bender::URStop::Response& res)
  */
 bool ur_move_to_q(bender::URMoveToQ::Request& req, bender::URMoveToQ::Response& res)
 {
-	// de-scramble Q message
+	// unscramble Q message
 	rw::math::Q q(6, req.target.Q.data());
 	
 	// log message
@@ -109,15 +132,100 @@ bool ur_servo_to_q(bender::URServoToQ::Request& req, bender::URServoToQ::Respons
 
 
 
+/**
+ * @class URNodeConfiguration
+ * 
+ * @brief Contains ur_node configuration read from XML configuration file
+ */
+struct URNodeConfiguration
+{
+	/* data */
+	std::string id;
+	std::string hostIP;
+	unsigned hostPort;
+	std::string robotIP;
+	unsigned robotPort;
+	std::string script;
+	
+	/* constructors */
+	URNodeConfiguration() :
+		id("UR1"),
+		hostIP("192.168.2.8"),
+		hostPort(30002),
+		robotPort(30001),
+		script("urscript1.ur")
+	{ }
+	
+	static URNodeConfiguration& loadfromXML(std::string filename)
+	{
+		URNodeConfiguration* config = new URNodeConfiguration();
+		
+		try {
+			// get path
+			boost::filesystem::path p(filename);
+			std::string configPath = p.parent_path().string()+"/";
+			
+			boost::property_tree::ptree tree;
+			read_xml(filename, tree);
+			boost::property_tree::ptree root = tree.get_child("ur_node");
+
+			// read robot id
+			std::string id = root.get_child("id").get_value<std::string>();
+			boost::trim(id);
+			config->id = id;
+			
+			// read host ip
+			std::string hostIP = root.get_child("hostIP").get_value<std::string>();
+			boost::trim(hostIP);
+			config->hostIP = hostIP;
+			
+			// read host port
+			std::string hostPort = root.get_child("hostPort").get_value<std::string>();
+			boost::trim(hostPort);
+			config->hostPort = boost::lexical_cast<unsigned>(hostPort);
+			
+			// read robot ip
+			std::string robotIP = root.get_child("robotIP").get_value<std::string>();
+			boost::trim(robotIP);
+			config->robotIP = robotIP;
+			
+			// read robot port
+			std::string robotPort = root.get_child("robotPort").get_value<std::string>();
+			boost::trim(robotPort);
+			config->robotPort = boost::lexical_cast<unsigned>(robotPort);
+			
+			// read script
+			std::string script = root.get_child("script").get_value<std::string>();
+			boost::trim(script);
+			config->script = configPath + script;
+			
+		} catch (const ptree_error& e) {
+			// convert from parse errors to RobWork errors.
+			RW_THROW(e.what());
+		}
+		
+		return *config;
+	}
+	
+	friend std::ostream& operator<<(std::ostream& stream, const URNodeConfiguration& config)
+	{
+		stream << "CONFIGURATION:\n"
+			" * ID: " << config.id << "\n"
+			" * host IP/port: " << config.hostIP << ":" << config.hostPort << "\n"
+			" * robot IP/port: " << config.robotIP << ":" << config.robotPort << "\n"
+			" * script: " << config.script << std::endl;
+		
+		return stream;
+	}
+};
+
+
+
 int main(int argc, char* argv[])
 {
 	// configuration
-	std::string hostIp = "192.168.2.8"; // localhost IP address
-	std::string robotId = "UR1"; // robot ID
-	std::string robotIp = "192.168.2.4"; // IP address of the robot
-	unsigned urport = 30002; // port to use locally
-	unsigned port = 30001; // port to use on robot
-	std::string scriptFilename = "urscript1.ur"; // UR script filename
+	std::string configFile;
+	URNodeConfiguration config;
 	
 	// initialize ROS
 	ros::init(argc, argv, "ur_node");
@@ -125,32 +233,82 @@ int main(int argc, char* argv[])
 	// parse command line options
 	std::string usage = "This ROS node is responsible for communication & control of UR robot.\n\n"
 		"Usage:\n"
-		"rosrun bender ur_node";
+		"rosrun bender ur_node CONFIG";
+	// named options
 	options_description desc("Options");
 	desc.add_options()
 		("help,h", "help message")
-		("host", value<std::string>(&hostIp), "local machine IP")
-		("id", value<std::string>(&robotId)->required(), "robot ID (used for identification in ROS)")
-		("id", value<std::string>(&robotIp)->required(), "robot IP address")
-		("script", value<std::string>(&scriptFilename), "UR script filename (default: urscript.ur)")
+		("config", value<std::string>(&configFile), "configuration XML file")
+		("id", value<std::string>(&config.id), "robot ID (used for identification in ROS)")
+		("hostip", value<std::string>(&config.hostIP), "local machine IP")
+		("hostport", value<unsigned>(&config.hostPort), "local machine port")
+		("robotip", value<std::string>(&config.robotIP), "robot IP")
+		("hostport", value<unsigned>(&config.robotPort), "robot port")
+		("script", value<std::string>(&config.script), "UR script filename")
 	;
-	variables_map vm;	
+	// positional options
+	positional_options_description pos_desc;
+	pos_desc.add("config", -1);
+	
+	// parse options
+	variables_map vm;
+	try {
+		store(command_line_parser(argc, argv).options(desc).positional(pos_desc).run(), vm);
+		notify(vm);
+		
+		if (vm.count("config")) {
+			config = URNodeConfiguration::loadfromXML(vm["config"].as<std::string>());
+		}
+		
+		if (vm.count("id")) {
+			config.id = vm["id"].as<std::string>();
+		}
+		
+		if (vm.count("hostip")) {
+			config.hostIP = vm["hostip"].as<std::string>();
+		}
+		
+		if (vm.count("hostport")) {
+			config.hostPort = vm["hostport"].as<unsigned>();
+		}
+		
+		if (vm.count("robotip")) {
+			config.robotIP = vm["robotip"].as<std::string>();
+		}
+		
+		if (vm.count("robotport")) {
+			config.robotPort = vm["robotport"].as<unsigned>();
+		}
+		
+		if (vm.count("script")) {
+			config.script = vm["script"].as<std::string>();
+		}
+	} catch (...) {
+		std::cout << usage << std::endl;
+		std::cout << desc << std::endl;
+		
+		return -1;
+	}
+	
+	ROS_INFO("Starting ur_node...");
+	std::cout << config << std::endl;
 	
 	// establish robot communication
-	URInterface.connect(robotIp, urport);
-	URInterface.start("192.168.2.8", port, scriptFilename);
+	URInterface.connect(config.robotIP, config.robotPort);
+	URInterface.start(config.hostIP, config.hostPort, config.script);
 	
 	// configure ROS node
 	ros::NodeHandle nh("~");
 	
 	// advertise services
+	ros::ServiceServer urGetQService = nh.advertiseService("ur_get_q", ur_get_q);
 	ros::ServiceServer urStopService = nh.advertiseService("ur_stop", ur_stop);
 	ros::ServiceServer urMoveQService = nh.advertiseService("ur_move_to_q", ur_move_to_q);
 	ros::ServiceServer urMoveTService = nh.advertiseService("ur_move_to_t", ur_move_to_t);
 	ros::ServiceServer urServoQService = nh.advertiseService("ur_servo_to_q", ur_servo_to_q);
 
 	// wait for calls
-	ROS_INFO("Started ur_node.");
+	ROS_INFO("Started.");
 	ros::spin();
 	
 	return 0;
